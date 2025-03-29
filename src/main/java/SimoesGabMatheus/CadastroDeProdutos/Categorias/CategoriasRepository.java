3ce9de618c429e1e25dc276f80339362729f820e
matheusgabsimoes <mtsgabriel.simoes@gmail.com>
2025-03-29 14:52:42 -0300

    feat: ProdutosService, ProdutosRepository e CategoriasRepository

---
+++ src/main/java/SimoesGabMatheus/CadastroDeProdutos/Categorias/CategoriasRepository.java
@@ -0,0 +1,6 @@
+package SimoesGabMatheus.CadastroDeProdutos.Categorias;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriasRepository extends JpaRepository<CategoriasModel, Long> {
+}
---
+++ src/main/java/SimoesGabMatheus/CadastroDeProdutos/Produtos/ProdutosService.java
@@ -0,0 +1,4 @@
+package SimoesGabMatheus.CadastroDeProdutos.Produtos;
+
+public class ProdutosService {
+}
