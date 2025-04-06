384ad5cb66fe40cd7801539e6cb32b3893d8645d
matheusgabsimoes <mtsgabriel.simoes@gmail.com>
2025-04-06 10:18:58 -0300

    fix: Altera ProdutosRepository de Class para Interface

---
+++ src/main/java/SimoesGabMatheus/CadastroDeProdutos/Produtos/ProdutosRepository.java
@@ -0,0 +1,6 @@
+package SimoesGabMatheus.CadastroDeProdutos.Produtos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutosRepository extends JpaRepository<ProdutosModel, Long> {
+}
