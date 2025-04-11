8fe08ed5006ae3241c909eba3bf8ac65ea88d3bd
matheusgabsimoes <mtsgabriel.simoes@gmail.com>
2025-04-11 20:32:53 -0300

    fix: Remove os comentarios desnecessarios

--- src/main/java/SimoesGabMatheus/CadastroDeProdutos/Produtos/ProdutosController.java
+++ src/main/java/SimoesGabMatheus/CadastroDeProdutos/Produtos/ProdutosController.java
@@ -14,31 +14,26 @@ public class ProdutosController {
         this.produtosService = produtosService;
     }
 
-    //Adicionar produtos
     @PostMapping("/criar")
     public ProdutosModel criarProduto(@RequestBody ProdutosModel produto) {
         return produtosService.criarProduto(produto);
     }
 
-    //Procurar produtos por ID
     @GetMapping("/listar")
     public List<ProdutosModel> mostrarProdutos() {
         return produtosService.listarProdutos();
     }
 
-    //Mostrar produtos por ID
     @GetMapping("/listar/{id}")
     public ProdutosModel listarProdutosPorId(@PathVariable Long id) {
         return produtosService.listarProdutosPorId(id);
     }
 
-    //Alterar dados dos produtos
     @PutMapping("/alterar/{id}")
     public ProdutosModel alterarProdutosPorId(@PathVariable Long id, @RequestBody ProdutosModel produtoAtualizado) {
         return produtosService.atualizarProduto(id, produtoAtualizado);
     }
 
-    //Remover produtos
     @DeleteMapping("/deletar/{id}")
     public void deletarProdutos(@PathVariable Long id) {
         produtosService.deletarProdutoPorId(id);
